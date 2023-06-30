d3672f6681a7717c403a3a0371e088c3c43c39af
NicolaCorea <nicola.corea.dev@gmail.com>
2023-06-30 12:53:07 +0200

    config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-// import { SolanaContractConnection } from "./networks/connections/sol-contract-connection";
-
+export * from "./defaults/default-connections";
 export * from "./defaults/web3-resolver";
+export * from "./defaults/erc721.ud.proxy.abi";
 
 export * from "./networks/connections/connection-library";
 export * from "./networks/connections/contract-connection";
 export * from "./networks/connections/contract-connection.types";
 export * from "./networks/connections/network-connection.types";
 export * from "./networks/connections/contract";
-// export * from "./networks/connections/sol-contract-connection";
 
 export * from "./resolver-providers/resolver-provider.interface";
+export * from "./resolver-providers/resolver-provider-router";
 export * from "./resolver-providers/providers/ens/ens-resolver-provider";
+export * from "./resolver-providers/providers/ens/ens-resolver-provider.consts";
+export * from "./resolver-providers/providers/freename/freename-resolver-provider.types";
+export * from "./resolver-providers/providers/freename/freename-resolver-provider.consts";
+export * from "./resolver-providers/providers/freename/freename-resolver-tools";
 export * from "./resolver-providers/providers/freename/freename-resolver-provider";
+export * from "./resolver-providers/providers/ud/ud-resolver-provider.consts";
+export * from "./resolver-providers/providers/ud/ud-resolver-tools";
 export * from "./resolver-providers/providers/ud/ud-resolver-provider";
 export * from "./resolver-providers/providers/base-resolver-provider";
 
@@ -21,8 +27,6 @@ export * from "./resolvers/resolved-resource/resolved-resource";
 export * from "./resolvers/types/resolved-resource-type";
 export * from "./resolvers/types/resolver-name";
 
+export * from "./tools/api-caller";
 export * from "./tools/name-tools";
 export * from "./tools/name-tools.types";
-
-// const sol = new SolanaContractConnection({ network: { networkName: "solana", rpcUrl: "null" }, address: "null" });
-// sol.findNft();
