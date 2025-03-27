3cf0f0a21212ed8af4f303b5b63434d2d1f01200
Mattia Lanzi <mattia.lanzi@freename.io>
2025-03-27 15:52:29 +0100

    Add Abstract and Abstract Sepolia Testnet support

--- src/shared/constants/freename-config.constants.ts
+++ src/shared/constants/freename-config.constants.ts
@@ -1,260 +1,327 @@
-import { FreenameContractConfig } from "../types/freename-resolver-provider.types";
-import { FreenameNetwork } from "../enumerations/enumerations";
-import { FNS_ABI } from "./freename-abi.constants";
+import {FreenameContractConfig} from "../types/freename-resolver-provider.types";
+import {FreenameNetwork} from "../enumerations/enumerations";
+import {FNS_ABI} from "./freename-abi.constants";
 
 export const FREENAME_CONTRACT_CONFS: FreenameContractConfig[] = [
-  {
-    // resolves to amoy (mumbai deprecated)
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.POLYGON_MUMBAI,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    // resolves to amoy (mumbai deprecated)
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.POLYGON_MUMBAI,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.POLYGON_AMOY,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.POLYGON_AMOY,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.POLYGON,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.POLYGON,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.CRONOS,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.CRONOS,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.BSC,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.BSC,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.AURORA,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.AURORA,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.BASE,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.BASE,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.BASE_SEPOLIA,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.BASE_SEPOLIA,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
 
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.SEI,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.SEI,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.SEI_TESTNET,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.SEI_TESTNET,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
+    // MAINNET
+    // ABSTRACT
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.ABSTRACT,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.ABSTRACT,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
 
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.ETHERLINK,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.ETHERLINK,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0xB8257763c36c48a301A763277F6064D729fE4623",
-    networkName: FreenameNetwork.ETHERLINK_TESTNET,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0xB8257763c36c48a301A763277F6064D729fE4623",
-    networkName: FreenameNetwork.ETHERLINK_TESTNET,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
+    // AURORA
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.AURORA,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.AURORA,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
 
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.CHILIZ,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.CHILIZ,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x12D5dFDf88fEa9397Fcb3d24bCbE1781d282298A",
-    networkName: FreenameNetwork.CHILIZ_TESTNET,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x12D5dFDf88fEa9397Fcb3d24bCbE1781d282298A",
-    networkName: FreenameNetwork.CHILIZ_TESTNET,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
+    // AVALANCHE
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.AVALANCHE,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.AVALANCHE,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
 
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.AVALANCHE,
-    test: false,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
-    networkName: FreenameNetwork.AVALANCHE,
-    test: false,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.AVALANCHE_FUJI,
-    test: true,
-    type: "read",
-    abi: FNS_ABI,
-  },
-  {
-    address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
-    networkName: FreenameNetwork.AVALANCHE_FUJI,
-    test: true,
-    type: "write",
-    abi: FNS_ABI,
-  },
-  {
-    address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
-    networkName: FreenameNetwork.SOLANA,
-    test: false,
-    type: "read",
-  },
-  {
-    address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
-    networkName: FreenameNetwork.SOLANA,
-    test: false,
-    type: "write",
-  },
-  {
-    address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
-    networkName: FreenameNetwork.SOLANA_DEVNET,
-    test: true,
-    type: "read",
-  },
-  {
-    address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
-    networkName: FreenameNetwork.SOLANA_DEVNET,
-    test: true,
-    type: "write",
-  },
+    // BASE
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.BASE,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.BASE,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // BSC
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.BSC,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.BSC,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // CHILIZ
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.CHILIZ,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.CHILIZ,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // CRONOS
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.CRONOS,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.CRONOS,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // ETHERLINK
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.ETHERLINK,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.ETHERLINK,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // POLYGON
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.POLYGON,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.POLYGON,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // SEI
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.SEI,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x465ea4967479A96D4490d575b5a6cC2B4A4BEE65",
+        networkName: FreenameNetwork.SEI,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // SOLANA
+    {
+        address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
+        networkName: FreenameNetwork.SOLANA,
+        test: false,
+        type: "read",
+    },
+    {
+        address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
+        networkName: FreenameNetwork.SOLANA,
+        test: false,
+        type: "write",
+    },
+
+
+    // TESTNET
+    // ABSTRACT SEPOLIA TESTNET
+    {
+        address: "0x9108a3F6629D247e410BaE0cb09636BA5Efa4631",
+        networkName: FreenameNetwork.ABSTRACT_SEPOLIA_TESTNET,
+        test: false,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x9108a3F6629D247e410BaE0cb09636BA5Efa4631",
+        networkName: FreenameNetwork.ABSTRACT_SEPOLIA_TESTNET,
+        test: false,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // AVALANCHE FUJI
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.AVALANCHE_FUJI,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.AVALANCHE_FUJI,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // BASE SEPOLIA
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.BASE_SEPOLIA,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.BASE_SEPOLIA,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // CHILIZ TESTNET
+    {
+        address: "0x12D5dFDf88fEa9397Fcb3d24bCbE1781d282298A",
+        networkName: FreenameNetwork.CHILIZ_TESTNET,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x12D5dFDf88fEa9397Fcb3d24bCbE1781d282298A",
+        networkName: FreenameNetwork.CHILIZ_TESTNET,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // ETHERLINK TESTNET
+    {
+        address: "0xB8257763c36c48a301A763277F6064D729fE4623",
+        networkName: FreenameNetwork.ETHERLINK_TESTNET,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0xB8257763c36c48a301A763277F6064D729fE4623",
+        networkName: FreenameNetwork.ETHERLINK_TESTNET,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // POLYGON AMOY
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.POLYGON_AMOY,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.POLYGON_AMOY,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // POLYGON MUMBAI
+    {
+        // resolves to amoy (mumbai deprecated)
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.POLYGON_MUMBAI,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        // resolves to amoy (mumbai deprecated)
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.POLYGON_MUMBAI,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // SEI TESTNET
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.SEI_TESTNET,
+        test: true,
+        type: "read",
+        abi: FNS_ABI,
+    },
+    {
+        address: "0x490Bfb43b44Ae54e36818Ed295B0814B2dEef2cC",
+        networkName: FreenameNetwork.SEI_TESTNET,
+        test: true,
+        type: "write",
+        abi: FNS_ABI,
+    },
+
+    // SOLANA DEVNET
+    {
+        address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
+        networkName: FreenameNetwork.SOLANA_DEVNET,
+        test: true,
+        type: "read",
+    },
+    {
+        address: "6cMUj75fcW7kaCJbFcSuAGjES22RMfnxg8QX8FJEprPL",
+        networkName: FreenameNetwork.SOLANA_DEVNET,
+        test: true,
+        type: "write",
+    },
 ];
--- src/shared/enumerations/enumerations.ts
+++ src/shared/enumerations/enumerations.ts
@@ -6,9 +6,9 @@
  * The name of the provider. Eg. 'Freename'
  */
 export declare const enum ProviderName {
-	FREENAME = "freename",
-	UD = "unstoppable",
-	ENS = "ens",
+    FREENAME = "freename",
+    UD = "unstoppable",
+    ENS = "ens",
 }
 
 /********************* NETWORKS *********************/
@@ -17,67 +17,73 @@ export declare const enum ProviderName {
  * All supported networks.
  */
 export declare const enum NetworkName {
-	POLYGON = "polygon",
-	POLYGON_MUMBAI = "polygon-mumbai",
-	ETHEREUM = "ethereum",
-	BSC = "bsc",
-	ZILLIQA = "zil",
-	HARDHAT = "hardhat",
-	AURORA = "aurora",
-	CRONOS = "cronos",
-	BASE = "base",
-	BASE_SEPOLIA = "base-sepolia",
-	AVALANCHE = "avalanche",
-	AVALANCHE_FUJI = "avalanche-fuji",
-	SEI = "sei",
-	SEI_TESTNET = "sei-testnet",
-	ETHERLINK = "etherlink",
-	ETHERLINK_TESTNET = "etherlink-testnet",
-	CHILIZ = "chiliz",
-	CHILIZ_TESTNET = "chiliz-testnet",
-	SOLANA_DEVNET = "solana-devnet",
-	SOLANA = "solana",
+    POLYGON = "polygon",
+    POLYGON_MUMBAI = "polygon-mumbai",
+    ETHEREUM = "ethereum",
+    BSC = "bsc",
+    ZILLIQA = "zil",
+    HARDHAT = "hardhat",
+    AURORA = "aurora",
+    CRONOS = "cronos",
+    BASE = "base",
+    BASE_SEPOLIA = "base-sepolia",
+    AVALANCHE = "avalanche",
+    AVALANCHE_FUJI = "avalanche-fuji",
+    SEI = "sei",
+    SEI_TESTNET = "sei-testnet",
+    ETHERLINK = "etherlink",
+    ETHERLINK_TESTNET = "etherlink-testnet",
+    CHILIZ = "chiliz",
+    CHILIZ_TESTNET = "chiliz-testnet",
+    SOLANA_DEVNET = "solana-devnet",
+    SOLANA = "solana",
 }
 
 /**
  * Networks supported by Freename.
  */
 export declare const enum FreenameNetwork {
-	POLYGON = "polygon",
-	POLYGON_MUMBAI = "polygon-mumbai",
-	POLYGON_AMOY = "polygon-amoy",
-	ETHEREUM = "ethereum",
-	BSC = "bsc",
-	CRONOS = "cronos",
-	SOLANA = "solana",
-	SOLANA_DEVNET = "solana-devnet",
-	AURORA = "aurora",
-	BASE = "base",
-	BASE_SEPOLIA = "base-sepolia",
-	SEI = "sei",
-	SEI_TESTNET = "sei-testnet",
-	ETHERLINK = "etherlink",
-	ETHERLINK_TESTNET = "etherlink-testnet",
+
+    // MAINNET
+    ABSTRACT = "abstract",
+    AURORA = "aurora",
+    AVALANCHE = "avalanche",
+    BASE = "base",
+    BSC = "bsc",
     CHILIZ = "chiliz",
+    CRONOS = "cronos",
+    ETHEREUM = "ethereum",
+    ETHERLINK = "etherlink",
+    POLYGON = "polygon",
+    SEI = "sei",
+    SOLANA = "solana",
+
+    // TESTNET
+    ABSTRACT_SEPOLIA_TESTNET = "abstract-sepolia-testnet",
+    AVALANCHE_FUJI = "avalanche-fuji",
+    BASE_SEPOLIA = "base-sepolia",
     CHILIZ_TESTNET = "chiliz-testnet",
-	AVALANCHE = "avalanche",
-	AVALANCHE_FUJI = "avalanche-fuji",
+    ETHERLINK_TESTNET = "etherlink-testnet",
+    POLYGON_AMOY = "polygon-amoy",
+    POLYGON_MUMBAI = "polygon-mumbai",
+    SEI_TESTNET = "sei-testnet",
+    SOLANA_DEVNET = "solana-devnet",
 }
 
 /**
  * Networks supported by Unstoppable Domains.
  */
 export declare const enum UdNetwork {
-	POLYGON = "polygon",
-	ETHEREUM = "ethereum",
-	ZILLIQA = "zil",
+    POLYGON = "polygon",
+    ETHEREUM = "ethereum",
+    ZILLIQA = "zil",
 }
 
 /**
  * Networks supported by ENS.
  */
 export declare const enum ENSEthNetwork {
-	ETHEREUM = "ethereum",
+    ETHEREUM = "ethereum",
 }
 
 /********************* ITEMS *********************/
@@ -86,25 +92,25 @@ export declare const enum ENSEthNetwork {
  * Item types supported by Freename.
  */
 export declare const enum FreenameItemType {
-	TLD = "TLD",
-	SECOND_LEVEL_DOMAIN = "SECOND_LEVEL_DOMAIN",
-	SUB_DOMAINED_DOMAIN = "SUB_DOMAINED_DOMAIN",
+    TLD = "TLD",
+    SECOND_LEVEL_DOMAIN = "SECOND_LEVEL_DOMAIN",
+    SUB_DOMAINED_DOMAIN = "SUB_DOMAINED_DOMAIN",
 }
 
 /**
  * Resolved resource types.
  */
 export declare const enum ResolvedResourceType {
-	TLD = "tld",
-	SECOND_LEVEL_DOMAIN = "domain",
-	UNTYPED = "UNTYPED",
+    TLD = "tld",
+    SECOND_LEVEL_DOMAIN = "domain",
+    UNTYPED = "UNTYPED",
 }
 
 /**
  * Name types
  */
 export declare const enum NameType {
-	TLD = "tld",
-	SECOND_LEVEL_DOMAIN = "domain",
-	SUB_DOMAINED_DOMAIN = "sub-domain",
+    TLD = "tld",
+    SECOND_LEVEL_DOMAIN = "domain",
+    SUB_DOMAINED_DOMAIN = "sub-domain",
 }
